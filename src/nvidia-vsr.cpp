b976be09aa951b6ce75d25f10f16b96d76491254
OBS RTX Dev <obs-dev@example.com>
2026-09-24 23:20:54 +0900

    Remove SHARED and UAV flags from ARGB textures for NvOFFRUC

--- src/nvidia-vsr.cpp
+++ src/nvidia-vsr.cpp
@@ -114,8 +114,7 @@ bool NvidiaVSR::Initialize(Microsoft::WRL::ComPtr<ID3D11Device> d3d11_device,
     desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
     desc.SampleDesc.Count = 1;
     desc.Usage = D3D11_USAGE_DEFAULT;
-    desc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_UNORDERED_ACCESS;
-    desc.MiscFlags = D3D11_RESOURCE_MISC_SHARED;
+    desc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
     
     for (int i = 0; i < 3; i++) {
         HRESULT hr = m_device->CreateTexture2D(&desc, nullptr, &m_fruc_tex[i]);
